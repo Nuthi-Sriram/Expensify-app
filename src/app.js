0be74094b083e721ced4dcfccc27e099b19ee36b
Nuthi-Sriram <sriramnuthi@gmail.com>
2019-09-02 10:19:43 +0530

    Added more modules

--- src/app.js
+++ src/app.js
@@ -1,31 +1,22 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
-import "normalize.css/normalize.css";
-import "./styles/styles.scss";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import AppRouter from './routers/AppRouter';
+import configureStore from './store/configureStore';
+import { addExpense } from './actions/expenses';
+import { setTextFilter } from './actions/filters';
+import getVisibleExpenses from './selectors/expenses';
+import 'normalize.css/normalize.css';
+import './styles/styles.scss';
 
-const ExpenseDashboardPage = () => (
-  <div>this is from the dashboard component</div>
+const store = configureStore();
+
+
+
+const jsx = (
+  <Provider store={store}>
+    <AppRouter />
+  </Provider>
 );
 
-const AddExpensePage = () => <div>this is from add expense page</div>;
-
-const EditExpensePage = () => (
-  <div>This is from the expense page</div>
-)
-const HelpPage = () => (<div>this is from the Help page</div>)
-const NotFoundPage = () => (<div>404!</div>)
-
-const routes = (
-  <BrowserRouter>
-    <Switch>
-      <Route path="/" component={ExpenseDashboardPage} exact={true} />
-      <Route path="/create" component={AddExpensePage} />
-      <Route path="/edit" component={EditExpensePage} />
-      <Route path="/help" component={HelpPage} />
-      <Route component={NotFoundPage} />
-    </Switch>
-  </BrowserRouter>
-);
-
-ReactDOM.render(routes, document.getElementById("app"));
+ReactDOM.render(jsx, document.getElementById('app'));
